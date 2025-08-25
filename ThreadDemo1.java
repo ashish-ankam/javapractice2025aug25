b77159cb6fed514de5acff6a1d90c3edc903aef9
ashish-ankam <ashishankam1999@gmail.com>
2025-08-25 21:59:12 +0530

    added extra print statements

--- ThreadDemo1.java
+++ ThreadDemo1.java
@@ -1,11 +1,13 @@
 public class ThreadDemo1 extends Thread{
     public void run(){
         System.out.println("Inside child thread");
+        System.out.println("overriden run method");
     }
 
     public static void main(String args[]){
         ThreadDemo1 t1 = new ThreadDemo1();
         t1.start();
         System.out.println("Inside main thread");
+        
     }
 }
