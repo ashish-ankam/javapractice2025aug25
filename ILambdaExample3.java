b94a9e5682aef6abfba476cbaf731b8083c69014
ashish-ankam <ashishankam1999@gmail.com>
2025-08-31 20:07:41 +0530

    written Lambda using new keyword

---
+++ ILambdaExample3.java
@@ -0,0 +1,3 @@
+public interface ILambdaExample3 {
+  double calculateArea(double radius);
+}
--- LambdaExampleImpl.java
+++ LambdaExampleImpl.java
@@ -1,4 +1,4 @@
-public class LambdaExampleImpl implements ILambdaExample ,ILambdaExample2{
+public class LambdaExampleImpl implements ILambdaExample ,ILambdaExample2, ILambdaExample3 {
 
 ILambdaExample lambdaExample = (message) -> System.out.println(message);
 
@@ -6,10 +6,22 @@ ILambdaExample2 lambdaExample2 = (a, b) -> {
     return a + b;
 };
 
+ILambdaExample3 lambdaExample3 = new ILambdaExample3() {
+    @Override
+    public double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+};
+
+
+
 public static void main(String[] args) {
     lambdaExample.display("Hello, Lambda Expressions!");
     int result = lambdaExample2.calculate(5, 10);
     System.out.println("Result: " + result);
+
+    double area = lambdaExample3.calculateArea(7);
+    System.out.println("Area: " + area);
 }
 
 }
