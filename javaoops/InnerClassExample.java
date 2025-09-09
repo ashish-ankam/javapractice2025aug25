2b280c26c4255f1a5e3659deced7f367241cb5fa
ashish-ankam <ashishankam1999@gmail.com>
2025-09-09 20:12:19 +0530

    comments added

--- javaoops/InnerClassExample.java
+++ javaoops/InnerClassExample.java
@@ -9,6 +9,11 @@ public class InnerClassExample {
     // Non-static inner class
     // Can access outer class members
     // Needs an instance of the outer class to be instantiated
+    // Example of encapsulation and organization of code
+    // Inner class defined within the scope of the outer class
+        // Good for logically grouping classes that are only used in one place
+        // Increases encapsulation
+        // Can access members of the outer class, including private ones
     
     class Inner {
         void display() {
